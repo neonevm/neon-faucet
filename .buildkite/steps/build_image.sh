b3431e62b67917fa0a930128ed394e573a587852
Andrey Voloshin <andrey@scalr.com>
2022-03-15 18:31:33 +0300

    Build without cache

--- .buildkite/steps/build_image.sh
+++ .buildkite/steps/build_image.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 #export FAUCET_REVISION=$(git rev-parse HEAD)
 
 #docker build -t neonlabsorg/faucet:$FAUCET_REVISION .
-docker build -t neonlabsorg/faucet:latest .
+docker build --no-cache=true -t neonlabsorg/faucet:latest .
 
 docker images
