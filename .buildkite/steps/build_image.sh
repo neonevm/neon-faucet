48598e3a6a2502e2f82dd1c8820c690fa853b93f
Andrey Voloshin <andrey@scalr.com>
2022-03-15 14:45:47 +0300

    set tag to latest

--- .buildkite/steps/build_image.sh
+++ .buildkite/steps/build_image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-export FAUCET_REVISION=$(git rev-parse HEAD)
+#export FAUCET_REVISION=$(git rev-parse HEAD)
 
-docker build -t neonlabsorg/faucet:$FAUCET_REVISION .
+#docker build -t neonlabsorg/faucet:$FAUCET_REVISION .
+docker build -t neonlabsorg/faucet:latest .
 
 docker images
--- .buildkite/steps/upload_image.sh
+++ .buildkite/steps/upload_image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-FAUCET_REVISION=$(git rev-parse HEAD)
-
+#FAUCET_REVISION=$(git rev-parse HEAD)
 
 docker login -u=${DHUBU} -p=${DHUBP}
-docker push neonlabsorg/faucet:$FAUCET_REVISION
+#docker push neonlabsorg/faucet:$FAUCET_REVISION
+docker push neonlabsorg/faucet:latest
