cd4a74130460f18a410ad649e2307b59a6334b16
Ivan Loboda <lobodatell@yandex.ru>
2022-04-22 15:44:06 +0300

    fix buld-image.sh script

--- .buildkite/steps/build_image.sh
+++ .buildkite/steps/build_image.sh
@@ -3,9 +3,6 @@ set -euo pipefail
 
 echo "Neon Faucet revision = ${BUILDKITE_COMMIT}"
 
-#export FAUCET_REVISION=$(git rev-parse HEAD)
-
-#docker build -t neonlabsorg/faucet:$FAUCET_REVISION .
-docker build --build-arg REVISION=${BUILDKITE_COMMIT} --no-cache=true -t neonlabsorg/faucet:latest .
+docker push neonlabsorg/faucet:${BUILDKITE_COMMIT}
 
 docker images
